fb57f07c545b995431d2baf74b69d4a14935f4a8
Alex Li <github@alexv525.com>
2025-08-15 11:41:09 +0800

    👷 Add the minimum Flutter requirement check to the action workflow

--- lib/src/extensions.dart
+++ lib/src/extensions.dart
@@ -42,49 +42,12 @@ extension BrightnessExtension on Brightness {
 
 /// Common extensions for the [Color].
 extension ColorExtension on Color {
-  static int _floatToInt8(double x) {
-    return (x * 255.0).round() & 0xff;
-  }
-
-  /// Returns a 32-bit value representing this color.
-  ///
-  /// The returned value is compatible with the default constructor
-  /// ([Color.new]) but does _not_ guarantee to result in the same color due to
-  /// [imprecisions in numeric conversions](https://en.wikipedia.org/wiki/Floating-point_error_mitigation).
-  ///
-  /// Unlike accessing the floating point equivalent channels individually
-  /// ([a], [r], [g], [b]), this method is intentionally _lossy_, and scales
-  /// each channel using `(channel * 255.0).round() & 0xff`.
-  ///
-  /// While useful for storing a 32-bit integer value, prefer accessing the
-  /// individual channels (and storing the double equivalent) where higher
-  /// precision is required.
-  ///
-  /// The bits are assigned as follows:
-  ///
-  /// * Bits 24-31 represents the [a] channel as an 8-bit unsigned integer.
-  /// * Bits 16-23 represents the [r] channel as an 8-bit unsigned integer.
-  /// * Bits 8-15 represents the [g] channel as an 8-bit unsigned integer.
-  /// * Bits 0-7 represents the [b] channel as an 8-bit unsigned integer.
-  ///
-  /// > [!WARNING]
-  /// > The value returned by this getter implicitly converts floating-point
-  /// > component values (such as `0.5`) into their 8-bit equivalent by using
-  /// > the [toARGB32] method; the returned value is not guaranteed to be stable
-  /// > across different platforms or executions due to the complexity of
-  /// > floating-point math.
-  int _toARGB32() {
-    return _floatToInt8(a) << 24 |
-        _floatToInt8(r) << 16 |
-        _floatToInt8(g) << 8 |
-        _floatToInt8(b) << 0;
-  }
-
   /// The alpha channel of this color in an 8 bit value.
   ///
   /// A value of 0 means this color is fully transparent. A value of 255 means
   /// this color is fully opaque.
-  int get _alpha => (0xff000000 & _toARGB32()) >> 24;
+  // ignore: deprecated_member_use
+  int get _alpha => (0xff000000 & value) >> 24;
 
   /// Determine the transparent color by 0 alpha.
   bool get isTransparent => _alpha == 0x00;
